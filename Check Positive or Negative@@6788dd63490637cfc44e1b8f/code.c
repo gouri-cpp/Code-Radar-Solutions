8764a05f835f67dee684babdf422885400fe3b7b
gouri-cpp <gouri0291.becse24@chitkara.edu.in>
2025-02-05 11:16:06 +0530

    Solution to Check Positive or Negative

---
+++ Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    int i;
+    scanf("%d",&i);
+    if(i>0){
+        printf("Positive");
+    }
+    else if(i<0){
+        printf("Negative");
+    }
+    else{
+        printf("Zero");
+    }
+    return 0;
+}
