71a8f3737b2f687ed66304f83559036247375046
gouri-cpp <gouri0291.becse24@chitkara.edu.in>
2025-02-07 14:48:54 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a ,&b);
+    char ch;
+    scanf("%f",&ch);
+    if(ch == +){
+        printf(a+b);
+    }
+    else if (ch == -){
+        printf(a-b);
+    }
+    else if (ch == *){
+        printf(a*b);
+    }
+    else if (ch == /){
+        printf(a-b);
+    }
+    return 0;
+}
