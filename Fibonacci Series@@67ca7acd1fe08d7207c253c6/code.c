25af5659602e1a4cc772bb785f79501b73f66582
gouri-cpp <gouri0291.becse24@chitkara.edu.in>
2025-03-10 10:38:41 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-int fibo(int n);
-int main(){
-   int n;
-   scanf("%d",&n);
-   printf("%d",fibo(n));
-   return 0;
-}
-int fibo(int n){
-   if(n == 0){
-      return 0;
-   }
-   if (n == 1){
-      return 0 ;
-   }
-   int fibonm1 = fibo(n-1);
-   int fibonm2 = fibo(n-2);
-   int fiboN = fibonm1 +fibonm2;
-   return fiboN;
 
+// Function to print Fibonacci series up to n terms
+void fibonacci(int n) {
+    int a = 0, b = 1, next;
+    
+    printf("Fibonacci Series: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    // printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    
+    fibonacci(n);
+    
+    return 0;
 }
